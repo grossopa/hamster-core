56bc186e2445588642f372a8e266e1f8eaf7164b
Jack Yin <grossopaforever@gmail.com>
2016-09-04 12:07:52 +0800

    WIP

--- hamster-core-api/src/main/java/org/hamster/core/api/environment/Environment.java
+++ hamster-core-api/src/main/java/org/hamster/core/api/environment/Environment.java
@@ -11,4 +11,36 @@ package org.hamster.core.api.environment;
  */
 public enum Environment {
     UNKNOWN, LOCAL, DEV, UAT, PROD;
+
+    private static Environment currentEnvironment;
+
+    /**
+     * initializes environment
+     * 
+     * @param environment
+     */
+    public static void initializeEnvironment(Environment environment) {
+        if (currentEnvironment != null) {
+            throw new IllegalArgumentException("Environment already set");
+        }
+        synchronized (Environment.class) {
+            currentEnvironment = environment;
+        }
+    }
+    
+    /**
+     * returns current environment, throws exception if environment not set
+     * 
+     * @return
+     */
+    public static final Environment current() {
+        if (currentEnvironment == null) {
+            throw new NullPointerException("Environment not set");
+        }
+        return currentEnvironment;
+    }
+    
+    public static final boolean isProd() {
+        return current() == Environment.PROD;
+    }
 }
